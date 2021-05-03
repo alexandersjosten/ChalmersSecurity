281c861caeb974b1df0962776aefaff6cedc2b3c
Sandro Stucki <sandro.stucki@gmail.com>
2021-05-03 15:16:15 +0200

    Tweak upload.sh.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
-RHOST=chalmerscid@remote11.chalmers.se
-RPATH=/chalmers/groups/security/www/www.cse.chalmers.se
+# Change this if your username is not your Chalmers CID
+CID=${USER}
+RHOST="${CID}@remote11.chalmers.se"
+RPATH="/chalmers/groups/security/www/www.cse.chalmers.se"
 rsync -priv --checksum $* \
   _site/ \
-  ${RHOST}:${RPATH}
+  "${RHOST}:${RPATH}"
