b277bc06d6205cb6b0cf1dbbba77ae4f02fb38ee
Sandro Stucki <sandro.stucki@gmail.com>
2021-06-08 10:29:50 +0200

    Add option for specifying username to 'upload.sh'.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,27 @@
-# Change this if your username is not your Chalmers CID
-CID=${USER}
+# Run as
+#
+#   ./upload.sh [-u CID] [OPTIONS]
+#
+# If omitted 'CID' defaults to $USER.
+# Remaining OPTIONS will be passed on to rsync.
+
+CID="${USER}"
+USAGE="USAGE: $0 [-u CID] [OPTIONS]"
+if [ "$1" = "-h" ]; then
+    echo "$USAGE" >&2
+    exit 0
+elif [ "$1" = "-u" ]; then
+    shift
+    if [ -z "$1" ]; then
+        echo "Missing CID!" >&2
+        echo "$USAGE" >&2
+        exit 1
+    else
+        CID="$1"
+    fi
+    shift
+fi
+echo "Using '${CID}' as CID." >&2
 RHOST="${CID}@remote11.chalmers.se"
 RPATH="/chalmers/groups/security/www/www.cse.chalmers.se"
 rsync -priv --checksum $* \
